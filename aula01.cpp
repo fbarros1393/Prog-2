7555ef47b74b433aa4d63596ebb3d342f9d5d09a
Felix barros <felix.barros@sou.ufmt.br>
2026-03-26 15:02:46 -0300

    Add files via upload

---
+++ aula01.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Bem Vindo ao C++!" <<std::endl;
+    return 0;
+}
---
+++ exe02.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    cout << "FELIX DE JESUS SOUSA BARROS" << endl;
+    return 0;
+}
