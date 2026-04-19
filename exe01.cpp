f9cc62f0f87b24444df1cee502e4ce46c50f0c33
Felix barros <felix.barros@sou.ufmt.br>
2026-04-19 04:23:31 -0300

    Add files via upload
    aula3 prog 2

--- exe01.cpp
+++ exe01.cpp
@@ -1,18 +1,71 @@
-//Exercicio 1: Desenvolva um programa que leia o nome completo de um usuário e sua idade,
-//e exiba essas informações de forma organizada na tela, ultilizando "cin", "getline" e "cout".
+//Sistemas de validação com regras encadeadas
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main () {
-    string nomecompleto;
-    int idade;
-    cout << "Digite seu nome completo: ";
-    getline(cin, nomecompleto);
-    cout <<"Digite sua idade: ";
+int main() {
+    int idade, nivel;
+    int autorizacao;
+
+    //Dados
+    cout << " SISTEMA DE VALIDAÇÃO COM REGRAS ENCADEADAS  " << endl;
+    cout << "Digite sua idade: ";
     cin >> idade;
 
-    cout << "Nome completo: " << nomecompleto << endl;
-    cout << "Idade: " << idade << endl;
+    if (idade < 16) {
+        cout << "\nAcesso Negado!" << endl;
+        cout << "Idade minima para acesso é 16 anos." << endl;
+        return 0;
+    }
+
+    else if (idade >=16 && idade <= 17) {
+        cout << "Digite a autorização (1 para SIM/ 0 para NÃO): ";
+        cin >> autorizacao;
+
+        if (autorizacao == 1) {
+            cout << "\n Acesso permitido (com restrições)!" << endl;
+            cout << "Nivel básico(1)." << endl;
+            nivel = 1;
+
+        } else {
+            cout << "\nAcesso negado!." << endl;
+            cout << "Para idade de 16 e 17 anos e necessário autorização." << endl;
+            return 0;
+        }
+    }
+    else if (idade >= 18) {
+        cout << "Digite o nível de acesso (1- Básico, 2 Intermediário, 3- Total): ";
+        cin >> nivel;
+
+        switch (nivel) {
+            case 1:
+                cout << "\nAcesso permitido!" << endl;
+                cout << "Nivel: Básico (1)" << endl;
+                break;
+            case 2:
+                cout << "\nAcesso permitido!" << endl;
+                cout << "Nivel intermediaário (2)" << endl;
+                break;
+            case 3:
+                cout << "\nAcesso permitido!" << endl;
+                cout << "Nivel: Total (3)" << endl;
+                break;
+            default:
+                cout << "\nErro: Nivel inválido!" << endl;
+                cout << "Niveis disponiveis (1), (2), (3). " << endl;
+                return 0;
+        
+
+        }
+    }
+    cout << "Idade: " << idade << " anos" << endl;
+
+    if (idade < 18) {
+        cout << "Acesso com restrições(menor de idade)" << endl;
+
+    } else {
+        cout << "Acesso completo " << endl;
+
+    }
+    cout << "Acesso concedido com sucesso!" << endl;
     return 0;
 }
